9b06377f7afb00f0779d70396d873fc410a7cd4b
myusername <gaurav.nakhale@neosoftmail.com>
2021-07-14 15:36:09 +0530

    latest changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 
     public function showAlluser()
     {
-            $showusers=User::all();
+            $showusers=User::orderBy('id', 'DESC')->get();
 
             return response()->json($showusers);
        
@@ -50,23 +50,35 @@ else{
         $User = User::create($request->all()); 
         $response['UserInsert'] = $User;
         $response['Success'] = 'User Added successfully!!!';
-        return response()->json($response, 201);
+        return response()->json($response,       201);
 }
 
     }
 
     public function update($id, Request $request)
     {
-            $this->validate($request, [
-                'name' => 'required|string|max:255',
-                        'phone_number' => 'required|max:10|min:10',
-                        'email' => 'required|email|max:255',
-                        'password' => 'required|string|min:6'
-            ]);
+                $validator = Validator::make($request->all(),
+                [  'name' => 'required|string|max:255',
+                'phone_number' => 'required|max:10|min:10',
+                'email' => 'required|email|max:255',
+                'password' => 'required|string|min:6'
+                ]
+        );
+                        $response['status'] = 1;
+                        if ($validator->fails()) {
+                                        $response['status'] = 0;
+                                        $response['errors'] = $validator->errors();
+                                        return response()->json($response);
+                                }
+else{
 
-            $User = User::findOrFail($id);
-            $User->update($request->all());
-            return response()->json($User, 200);
+        $User = User::findOrFail($id);
+        $User->update($request->all());
+        $response['UserUpdate'] = $User;
+        $response['Success'] = 'User Updated successfully!!!';
+        return response()->json($response, 200);
+
+}
        
     }
 
